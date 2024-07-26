19fca199196154a2e961b5050e1ec0475d0ee618
Komal Gupta <komgupta1@WKMZTA0ADD4F.global.publicisgroupe.net>
2024-07-26 17:54:46 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Star Wars Characters</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
